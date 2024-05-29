73ba580bb5b9d788864f6a509bb79605a9da5606
Daniele Senko <142120811+daniele-senko@users.noreply.github.com>
2024-05-28 23:56:48 -0400

    exercicio1_4

---
+++ fixing_ex/ex_p2/lab1_4.asm
@@ -0,0 +1,12 @@
+.text
+  # Inicialização dos valores
+    li $t0, 15      # Carrega 15 em $t0
+    li $t1, 22      # Carrega 22 em $t1
+    li $t2, 10      # Carrega 10 em $t2
+    li $t3, 3       # Carrega 3 em $t3
+    
+    add $t4, $t0, $t1
+    
+    sub $t5, $t2, $t3
+    
+    sub $t6, $t4, $t5
