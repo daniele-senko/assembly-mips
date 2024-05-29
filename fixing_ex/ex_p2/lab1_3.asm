f57d60c0dddd507d21b522005a58eaa4c8a61e10
Daniele Senko <142120811+daniele-senko@users.noreply.github.com>
2024-05-28 23:56:04 -0400

    exercicio1_3

---
+++ fixing_ex/ex_p2/lab1_3.asm
@@ -0,0 +1,16 @@
+.text 
+	#inicializando valores
+	li $t0, 12
+	addi $t1, $zero, 8
+	
+	#multiplicação de valores e armazenamento em $s0
+	mul $t2, $t0, $t1
+	
+	#soma e valores armazenados em $t3
+	add $t4, $t2, $t0
+
+	
+	
+	
+	
+	
