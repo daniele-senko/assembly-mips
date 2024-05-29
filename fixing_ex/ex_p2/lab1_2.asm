559bb9b490b58c836c6032796ab2435da450e6e7
Daniele Senko <142120811+daniele-senko@users.noreply.github.com>
2024-05-28 23:55:34 -0400

    exercicio1_2

---
+++ fixing_ex/ex_p2/lab1_2.asm
@@ -0,0 +1,11 @@
+.text
+	#inicializando valores
+	li $t0, 1092
+	li $t1, 100
+	li $t2, 8
+	
+	#sub $t0 e $t1, sendo armazenado em $t3
+	sub $t3, $t0, $t1
+	
+	#soma de $t3 com $t2
+	add $t4, $t3, $t2
